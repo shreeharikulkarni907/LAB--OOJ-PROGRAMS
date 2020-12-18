c92e60b2823d1f2c2e54dd78ca6cf62576cec76e
shreeharikulkarni907 <63466408+shreeharikulkarni907@users.noreply.github.com>
2020-12-18 11:23:59 +0530

    Add files via upload

--- lab11/extra/Random Numbers/random.java
+++ lab11/extra/Random Numbers/random.java
@@ -33,22 +33,29 @@ class cube implements Runnable
 		System.out.println("Cube Of The Numeber is " + num*num*num);
 	}
 }
-class ran
+class ran implements Runnable
 {
-	public static void main(String[] args)
+	String name;
+	Thread t;
+	int p;
+	int lim;
+	ran(String name)
 	{
-		int lim;
-		System.out.println("Enter the Limit Upto Which You Want To generate Cube And Square");
+		this .name=name;
+		t=new Thread(this,name);
+	}
+	public void run()
+	{
 		Scanner sc=new Scanner(System.in);
-		lim=sc.nextInt();
+        try
+	    {
+	    	System.out.println("Enter the Limit Upto Which You Want To Generate  The Random Number ");
+	    	lim=sc.nextInt();
+	    	for(int i=0;i<lim;i++)
+	    	{
 
-		try
-		{
-		   Thread.currentThread().setPriority(10);
-		   for(int i=0;i<lim;i++)
-		   {
 		       int p=(int)((10)*(Math.random()));
-		       Thread.sleep(1000);
+		       System.out.println("Random Number Generated is " + p);
 		       if(p%2==0)
 		       {
 			      square s=new square(p,"even");
@@ -56,13 +63,24 @@ class ran
 		       }
 		       else
 		       {
-		       	  cube c=new cube(p,"odd");
-		       	  c.t.start();
-		       	  //c.t.join();
-		       }
-		   }
+		          cube c=new cube(p,"odd");
+		          c.t.start();
+		         //c.t.join();
+	           } 
+	            Thread.sleep(1000);
+ 
+	       }
 		}catch(InterruptedException e){
 			System.out.println("Program Interrupted");
 		}
+    }
+}
+class random
+{
+	public static void main(String[] args)
+	{
+		ran r=new ran("RANDOM");
+		r.t.start(); 
+
 	}
 }
